731fc2671ab7d96c1ec2f6e61e5d00ab18ac1064
rohit <rohitrainbow@yahoo.co.in>
2019-06-17 21:30:56 +0530

    warning removed from java classes

--- src/main/java/store/theimitation/WebServices/controllers/ProductController.java
+++ src/main/java/store/theimitation/WebServices/controllers/ProductController.java
@@ -3,7 +3,6 @@ package store.theimitation.WebServices.controllers;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
