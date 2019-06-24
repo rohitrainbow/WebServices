e8c716379ae301571d229c755faf9b35ac513f99
rohitrainbow <rohitrainbow@yahoo.co.in>
2019-06-24 16:55:02 +0530

    Dist added login and register working

--- src/main/java/store/theimitation/WebServices/models/User.java
+++ src/main/java/store/theimitation/WebServices/models/User.java
@@ -1,7 +1,6 @@
 package store.theimitation.WebServices.models;
 
 import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
